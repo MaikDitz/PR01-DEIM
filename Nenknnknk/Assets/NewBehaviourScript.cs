490d3ca6ea408ce8152d87eccc6e5fa594e6a78f
Michael <maikditz@gmail.com>
2021-09-17 19:04:36 +0200

    Ejercicio 1.c
    Operaciones con variables aritméticas y concatenando.

--- Nenknnknk/Assets/NewBehaviourScript.cs
+++ Nenknnknk/Assets/NewBehaviourScript.cs
@@ -8,10 +8,14 @@ public class NewBehaviourScript : MonoBehaviour
     public string Var2 = "hola wenas";
     public float Var3 = 4f;
     public bool Var4 = true;
+    public int Suma1 = 5;
+    public int Suma2 = 7;
     // Start is called before the first frame update
     void Start()
     {
         print(Var1 + Var2 + Var3 + Var4);
+        print(Var1 + Var3);
+        print(Suma1 + Suma2);
     }
 
     // Update is called once per frame

948bbfa31a3460c3cb3640308ec03f21f4feba6c
Michael <maikditz@gmail.com>
2021-09-17 18:53:36 +0200

    Ejercicio 1.a
    Inserción de variables

--- Nenknnknk/Assets/NewBehaviourScript.cs
+++ Nenknnknk/Assets/NewBehaviourScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private int Var1 = 2;
+    private string Var2 = "hola wenas";
+    private float Var3 = 4f;
+    private bool Var4 = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(Var1 + Var2 + Var3 + Var4);
     }
 
     // Update is called once per frame

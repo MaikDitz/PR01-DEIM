d345a0cebeaee7aa1e9089ef2ce491fe06d54bfd
Maiki <maikditz@gmail.com>
2021-09-20 18:58:25 +0200

    Ejercicio 2.d
    Hacer que salga en consola números al azar hasta que encuentre el numero que le he puesto.

--- Nenknnknk/Assets/NewBehaviourScript.cs
+++ Nenknnknk/Assets/NewBehaviourScript.cs
@@ -3,32 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
-    public int Var1 = 2;
+    public int Var1 = 10;
     public string Var2 = "hola wenas";
     public float Var3 = 4f;
     public bool Var4 = true;
     public int Edad = 1;
+    public int Aleatorio;
     // Start is called before the first frame update
     void Start()
     {
-        for (; Edad < 19; Edad++)
+        while (Aleatorio != 5)
         {
-            if (Edad < 10)
-            {
-                print("Tas chiquito");
-            }
-
-            else if (Edad >= 10 && Edad < 18)
-            {
-                print("Tas medio chiquito");
-            }
-            else
-            {
-                print("Tas mayor");
-            }
+            Aleatorio = Random.Range(0, 10);
+            print("El numero es " + Aleatorio);
         }
-            
-
     }
     // Update is called once per frame
     void Update()

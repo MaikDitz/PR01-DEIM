edc094577d90e8ca8d3c198e4068bc61ef8477c4
Maik <MaikDitz@gmail.com>
2021-09-24 20:40:17 +0200

    Ejercicio en clase de movimiento con mando
    Scripts con nuestro objeto para moverlo con mando.

---
+++ Probando en clase/Assets/CameraMove.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] GameObject playerObject;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+  {
+       // MoverCamara();
+    }
+
+    void MoverCamara()
+    {
+        transform.LookAt(player);
+        float newX = playerObject.transform.position.x;
+        float newY = playerObject.transform.position.y;
+        float newZ = transform.position.z;
+        print(playerObject.transform.position);
+        transform.position = new Vector3(newX, newY, newZ);
+    }
+}
+
